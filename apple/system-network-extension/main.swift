ceb3ad7e15df6cf6898c5ed729aa01a4c838f654
Kevin Cox <kevincox@kevincox.ca>
2024-12-05 10:12:06 -0500

    Enable lib backtraces in network extension.
    This mostly applies when we create and print `anyhow::Error` objects.

--- apple/system-network-extension/main.swift
+++ apple/system-network-extension/main.swift
@@ -1,6 +1,10 @@
 import Foundation
 import NetworkExtension
 
+// TODO: Use `std::panic::set_backtrace_style()` in Rust initialization once stabilized.
+// https://doc.rust-lang.org/std/panic/fn.set_backtrace_style.html
+setenv("RUST_BACKTRACE", "1", 1)
+
 autoreleasepool {
     NEProvider.startSystemExtensionMode()
 }

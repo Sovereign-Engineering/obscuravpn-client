dca1f9388203aa3116861d13aba47d146920ecd7
Elijah Lopez <elijahllopezz@gmail.com>
2024-12-09 13:55:39 -0500

    webui: remove logviewer
    - this was from the old client

--- obscura-ui/src/views/index.ts
+++ obscura-ui/src/views/index.ts
@@ -4,7 +4,6 @@ export { default as DeveloperView } from './DeveloperView';
 export { default as Help } from './HelpView';
 export { default as Location } from './LocationView';
 export { default as LogIn } from './LogInView';
-export { default as LogViewer } from './LogViewer';
 export { default as FallbackAppRender } from './render-fallbacks/FallbackAppRender';
 export { default as Settings } from './Settings';
 export { default as SplashScreen } from './SplashScreen';

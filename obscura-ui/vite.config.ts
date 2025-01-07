640be6ed818f09aefe96bdbb685761077d8c371b
Elijah Lopez <elijahllopezz@gmail.com>
2025-01-07 16:15:40 -0500

    webui: cleanup deps

--- obscura-ui/vite.config.ts
+++ obscura-ui/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-// import topLevelAwait from 'vite-plugin-top-level-await';
 import { visualizer } from 'rollup-plugin-visualizer';
 // https://vitejs.dev/config/
 // https://tauri.app/v1/guides/getting-started/setup/vite#create-the-frontend
@@ -12,10 +11,6 @@ export default defineConfig({
   plugins: [
     svgr(),
     react(),
-    // topLevelAwait({
-    //   promiseExportName: '__TLA',
-    //   promiseImportName: i => `__TLA_${i}`
-    // }),
     visualizer(),
   ],
   // prevent vite from obscuring rust errors

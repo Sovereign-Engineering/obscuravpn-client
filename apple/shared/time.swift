ac181e27adfa1fb894e1a582a8934942db6c5986
Francesca Lovebloom <francesca@sovereign.engineering>
2025-10-07 07:47:02 -0700

    Avoid `DateFormatter` quirks on iOS (#726)
    Resolves https://linear.app/soveng/issue/OBS-2381/fix-timestamp-format-of-ios-debug-bundles
    
    Relevant documentation: https://developer.apple.com/documentation/foundation/dateformatter#Working-With-Fixed-Format-Date-Representations
    
    On iOS, `DateFormatter` may rewrite the specified format string to suit
    the user's locale.

--- apple/shared/time.swift
+++ apple/shared/time.swift
@@ -1,8 +1,3 @@
 import Foundation
 
-let utcDateFormat: DateFormatter = {
-    var f = DateFormatter()
-    f.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
-    f.timeZone = TimeZone.gmt
-    return f
-}()
+let utcDateFormat: ISO8601DateFormatter = ISO8601DateFormatter()

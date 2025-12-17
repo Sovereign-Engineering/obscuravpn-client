be33e6f437567db1f0f80e9d2e607f4f473c8be0
Florian Uekermann <florian@uekermann.me>
2025-12-17 17:02:28 +0100

    fix dns cache seed

--- rustlib/src/constants.rs
+++ rustlib/src/constants.rs
@@ -1,6 +1,8 @@
 // This file contains constants, which may need to be updated at some point
 
+use const_format::formatcp;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
-pub const DEFAULT_API_URL: &str = "https://v1.api.prod.obscura.net/api";
-pub const DNS_CACHE_SEED: &[(&str, &[SocketAddr])] = &[(DEFAULT_API_URL, &[SocketAddr::new(IpAddr::V4(Ipv4Addr::new(66, 42, 95, 12)), 0)])];
+pub const DEFAULT_API_DOMAIN: &str = "v1.api.prod.obscura.net";
+pub const DEFAULT_API_URL: &str = formatcp!("https://{DEFAULT_API_DOMAIN}/api");
+pub const DNS_CACHE_SEED: &[(&str, &[SocketAddr])] = &[(DEFAULT_API_DOMAIN, &[SocketAddr::new(IpAddr::V4(Ipv4Addr::new(66, 42, 95, 12)), 0)])];

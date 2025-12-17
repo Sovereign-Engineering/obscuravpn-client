5896f76ed110a58081a35dec150358c615232c95
Florian Uekermann <florian@uekermann.me>
2025-12-17 11:47:58 +0100

    seed DNS cache

--- rustlib/src/config/dns_cache.rs
+++ rustlib/src/config/dns_cache.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::net::SocketAddr;
 
-#[derive(Clone, Debug, Default, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct DnsCache {
     entries: HashMap<String, Vec<SocketAddr>>,
 }
@@ -14,5 +14,13 @@ impl DnsCache {
 
     pub fn set(&mut self, name: &str, addr: &[SocketAddr]) {
         self.entries.insert(name.to_string(), addr.to_vec());
+    }
+}
+
+impl Default for DnsCache {
+    fn default() -> Self {
+        Self {
+            entries: HashMap::from([("v1.api.prod.obscura.net".to_string(), vec![SocketAddr::from(([66, 42, 95, 12], 0))])]),
+        }
     }
 }

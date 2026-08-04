35fce1df3736a3d0f626da2f431780898924c351
Francesca Lovebloom <francesca@sovereign.engineering>
2026-08-04 14:00:08 -0700

    Apply safe area insets to splash screen (#1041)
    Resolves https://linear.app/soveng/issue/OBS-3930/splash-screen-doesnt-respect-safe-area
    
    As a bonus, I added our standard padding to the Debug Bundle card, since this previously looked bad on mobile.

--- obscura-ui/src/views/SplashScreen.tsx
+++ obscura-ui/src/views/SplashScreen.tsx
@@ -2,6 +2,7 @@ import { Group, Image, Loader, Stack, Text } from '@mantine/core';
 import { useThrottledValue } from '@mantine/hooks';
 import AppIcon from '../../../apple/client/Assets.xcassets/AppIcon.appiconset/icon_128x128.png';
 import { LinuxServiceDegradation, OsStatusWVpnStatus, WindowsServiceDegradation } from '../common/appContext';
+import commonClasses from '../common/common.module.css';
 import DebugBundle from '../components/DebugBundle';
 import ObscuraWordmark from '../components/ObscuraWordmark';
 import LinuxServiceDegraded from './LinuxServiceDegraded';
@@ -20,7 +21,7 @@ export default function SplashScreen({ osStatus, linuxDegradation, windowsDegrad
   // only show the debug bundle while loading after a prolonged period, but immediately when degraded
   const osStatusThrottled = useThrottledValue(osStatus, 5000);
   return (
-    <Stack h='100vh' align='center' justify='center' gap='xl'>
+    <Stack className={commonClasses.applyInsets} h='100vh' align='center' justify='center' gap='xl'>
       <Image src={AppIcon} w={64} />
       <ObscuraWordmark />
       {linuxDegradation !== undefined && <LinuxServiceDegraded degradation={linuxDegradation} />}
@@ -31,7 +32,9 @@ export default function SplashScreen({ osStatus, linuxDegradation, windowsDegrad
           <Loader size='xl' type='bars' />
         </Group>}
       {osStatus !== null && (degraded || osStatusThrottled !== null) &&
-        <DebugBundle osStatus={osStatus} />}
+        <div style={{ padding: '0 20px' }}>
+          <DebugBundle osStatus={osStatus} />
+        </div>}
     </Stack>
   );
 }

657d5677a5b2a5db997573b999789b3dc2056504
Florian Uekermann <florian@uekermann.me>
2026-08-31 17:43:19 +0000

    Linux: add per-step logging to tun device creation

--- rustlib/src/bin/obscura/service/os/linux/start_error.rs
+++ rustlib/src/bin/obscura/service/os/linux/start_error.rs
@@ -8,6 +8,8 @@ pub enum LinuxServiceStartError {
     NoDnsManager,
     #[error("Failed to set up nftables.")]
     NftablesSetup,
+    #[error("Failed to set up the TUN device.")]
+    TunSetup,
     #[error("Unexpected error. Details: {0}")]
     Unexpected(#[from] anyhow::Error),
 }
